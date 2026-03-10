ca8b16a1bdf99509d466e9d3b7a3fd95e27fe6dd
Hieupt <phamthehieu7@gmail.com>
2026-03-10 09:34:24 +0700

    Thêm cấu hình cho Angular CLI và cập nhật đường dẫn module. Xóa các file cấu hình không cần thiết và chuyển đổi import sang sử dụng alias cho các module trong dự án.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { appConfig } from '@/core/app.config';
+import { App } from '@/layout/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
